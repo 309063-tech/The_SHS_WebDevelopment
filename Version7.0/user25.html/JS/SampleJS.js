ffa745999dbd3f5790db2bc225fa7e7636f6459e
239667 <239667@shakopeeschools.org>
2026-05-13 09:10:36 -0500

    1.7 JS
    Javascript

--- Version7.0/user25.html/JS/SampleJS.js
+++ Version7.0/user25.html/JS/SampleJS.js
@@ -1,8 +1,10 @@
 function changeText() {
+
     document.getElementById("textChange").innerHTML =
-    "Thanks for liking my webpage!";
+        "Thanks for liking my webpage!";
 
     document.getElementById("textChange").classList.add("bigText");
+
 }
 
 
@@ -12,10 +14,11 @@ var pos = 0;
 var msg = "User 25's JavaScript Sample Code!";
 
 function Scroll() {
+
     document.title =
-    msg.substring(pos, msg.length) +
-    space +
-    msg.substring(0, pos);
+        msg.substring(pos, msg.length) +
+        space +
+        msg.substring(0, pos);
 
     pos++;
 
@@ -23,79 +26,103 @@ function Scroll() {
         pos = 0;
 
     window.setTimeout("Scroll()", 150);
+
 }
 
 Scroll();
 
 
+
 function showAlert() {
+
     alert("Hello! Welcome to my website!");
+
 }
 
 
 
 function doubleClick() {
+
     alert("You double clicked!");
+
 }
+
 
 
 function mouseOver() {
+
     document.getElementById("hoverText").innerHTML =
-    "You are hovering!";
+        "You are hovering!";
+
 }
+
 
 
 function mouseOut() {
+
     document.getElementById("hoverText").innerHTML =
-    "Hover over me";
+        "Hover over me";
+
 }
 
+
+
 function keyPress() {
+
     document.getElementById("keyText").innerHTML =
-    "You pressed a key!";
+        "You pressed a key!";
+
 }
+
 
 
 function inputChange() {
+
     document.getElementById("inputText").innerHTML =
-    "Input changed!";
+        "Input changed!";
+
 }
+
 
 
 window.onload = function () {
+
     document.getElementById("loadText").innerHTML =
-    "Page fully loaded!";
+        "Page fully loaded!";
+
 };
 
 
+
 function isKeyPressed(event) {
-  var text = "The shift key was NOT pressed!";
-  if (event.shiftKey == 1) {
-    text = "The shift key was pressed!";
-  }
-  document.getElementById("demo").innerHTML = text;
+
+    var text = "The shift key was NOT pressed!";
+
+    if (event.shiftKey == 1) {
+
+        text = "The shift key WAS pressed!";
+
+    }
+
+    document.getElementById("demo").innerHTML = text;
+
 }
 
 
 
+function showTime() {
+
+    const date = new Date();
+
+    document.getElementById("timeText").innerHTML =
+        date.toLocaleTimeString();
+
+}
 
 
 
+function changeBackground() {
 
+    document.body.style.backgroundColor = "lightblue";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
